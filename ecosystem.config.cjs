4f97f15718b49a85934c4c77229869bf7a65d30a
Ali Toshmatov <alitoshmatov2001@gmail.com>
2025-02-19 14:11:47 +0500

    Updated cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "voiceAI",
+      script: "index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "256M",
+      max_restarts: 5,
+      exp_backoff_restart_delay: 1000,
+      time: true,
+      env: {
+        NODE_ENV: "production",
+      },
+      // Resource limitations
+      node_args: "--max-old-space-size=256", // Limit Node.js memory to 256MB
+      cpu: "25%", // Limit CPU usage to 25%
+    },
+  ],
+};
